460bd54546e061577a699884d7175f93af3ce417
Cris <cristhianeliassales@gmail.com>
2025-10-01 14:02:51 -0300

    ex014

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var fr = document.getElementById('frase')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 6 && hora < 12) {
+        img.src = 'manha.png'
+        document.body.style.background = '#658bd5ff'
+        // Bom Dia
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'tarde.png'
+        document.body.style.background = '#b46829ff'
+        // Boa tarde
+        fr.innerHTML = '<p>Boa Tarde!!!</p>'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#232121ff'
+        // Boa Noite
+    }
+}

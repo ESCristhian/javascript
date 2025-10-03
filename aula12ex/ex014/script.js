c147acc7d0f3171ef49cf702c9c73abdb5b47ad3
Cris <cristhianeliassales@gmail.com>
2025-10-03 10:38:41 -0300

    .

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -4,10 +4,12 @@ function carregar() {
     var fr = document.getElementById('frase')
     var data = new Date()
     var hora = data.getHours()
+    hora = 13
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 6 && hora < 12) {
         img.src = 'manha.png'
         document.body.style.background = '#658bd5ff'
+        fr.innerHTML = '<p>Bom Dia!!!</p>'
         // Bom Dia
     } else if (hora >= 12 && hora < 18) {
         img.src = 'tarde.png'
@@ -17,6 +19,7 @@ function carregar() {
     } else {
         img.src = 'noite.png'
         document.body.style.background = '#232121ff'
+        fr.innerHTML = '<p>Boa Noite!!!</p>'
         // Boa Noite
     }
 }

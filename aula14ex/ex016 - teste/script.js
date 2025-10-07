d2f7f58b060ded0ea2c0e08f2f2a991dceaefc36
Cris <cristhianeliassales@gmail.com>
2025-10-07 10:56:31 -0300

    ex16

---
+++ aula14ex/ex016 - teste/script.js
@@ -0,0 +1,29 @@
+var ini = document.getElementById('inicio')
+var fim = document.getElementById('fim')
+var pas = document.getElementById('passo')
+var res = document.getElementById('res')
+
+function calcular() {
+    var ninicio = Number(ini.value)
+    var nfim = Number(fim.value)
+    var npasso = Number(pas.value)
+
+    if (ninicio == '') {
+        window.alert('Não foi possivel determinar o INÍCIO')
+        return
+    }
+
+    if (npasso <= 0) {
+        window.alert('Passso invalido! Considere PASSO: 1')
+        npasso = 1
+    }
+    res.innerHTML = ''
+    for (let conta = ninicio; conta <= nfim; conta += npasso) {
+        res.innerHTML += conta
+        if (conta + npasso <= nfim) {
+            res.innerHTML += ' 👉 '
+        } else {
+            res.innerHTML += ' 🏴 '
+        }
+    }
+}

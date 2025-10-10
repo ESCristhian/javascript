386825e4ef99a245e8872aa7a08baf1c2ab97228
Cris <cristhianeliassales@gmail.com>
2025-10-10 11:34:50 -0300

    aula16

---
+++ aula15/ambiente.js
@@ -0,0 +1,25 @@
+let valores = [1, 2, 3, 4, 5, 6]
+
+let pos = valores.indexOf(8 )
+if (pos == -1) {
+    console.log(`O valor não foi encontrado`)
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
+
+/* for (pos in valores) {
+    console.log(valores[pos])
+} */
+
+/* for (let pos = 0; pos < valores.length; pos++) {
+    console.log(valores[pos]);
+    
+} */
+
+
+/* let num = [5, 3, 1, 3, 6, 2]
+
+num.sort()
+console.log(`Esses são os elementos ${num}`)
+console.log(`Esse vetor tem ${num.length} elementos`)
+console.log(`O primeior valor é ${num[0]}`) */
